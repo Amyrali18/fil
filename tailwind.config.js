2469a01d7fa6c817175b47bcaaf00bee0a52a235
Amirali Niazi <amiraliniazi1401@gmail.com>
2024-08-28 12:48:18 +0330

    allah is with us update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [  '**.html', // فایل اصلی HTML
+   './public/javascript/**.js'],
+  theme: {
+    extend: {
+      screens:{
+        mobile:"0px",
+        desktop:"1024px"
+      },
+    },
+  },
+  plugins: [],
+}
+
